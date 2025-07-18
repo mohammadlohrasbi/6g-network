9ba552bf9b6268ae1b11a0626c53f75276522411
mohammadlohrasbi <46881172+mohammadlohrasbi@users.noreply.github.com>
2025-07-18 13:03:37 -0400

    Update utils.js

--- caliper-workspace/workload/utils.js
+++ caliper-workspace/workload/utils.js
@@ -2,9 +2,10 @@ const generateRandomID = (prefix, max) => {
     return `${prefix}${Math.floor(Math.random() * max)}`;
 };
 
-const generateRandomCoords = () => {
-    const x = (Math.random() * 180 - 90).toFixed(4); // Latitude [-90, 90]
-    const y = (Math.random() * 360 - 180).toFixed(4); // Longitude [-180, 180]
+const generateRandomCoords = (centerX = 0, centerY = 0, sideLength = 100) => {
+    const halfSide = sideLength / 2;
+    const x = (Math.random() * sideLength - halfSide + centerX).toFixed(4); // مختصات x در بازه [centerX - halfSide, centerX + halfSide]
+    const y = (Math.random() * sideLength - halfSide + centerY).toFixed(4); // مختصات y در بازه [centerY - halfSide, centerY + halfSide]
     return { x, y };
 };
 
