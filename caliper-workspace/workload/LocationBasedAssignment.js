bd86f67243873fe8ec216c5f52cda7f6e2911cfa
mohammadlohrasbi <46881172+mohammadlohrasbi@users.noreply.github.com>
2025-07-18 13:18:28 -0400

    Update LocationBasedAssignment.js

--- caliper-workspace/workload/LocationBasedAssignment.js
+++ caliper-workspace/workload/LocationBasedAssignment.js
@@ -1,29 +1,35 @@
+'use strict';
+
 const { WorkloadModuleBase } = require('@hyperledger/caliper-core');
+const { generateRandomID, generateRandomCoords } = require('./utils.js');
 
 class LocationBasedAssignmentWorkload extends WorkloadModuleBase {
     constructor() {
         super();
         this.chaincodeID = 'LocationBasedAssignment';
         this.channel = 'GeneralOperationsChannel';
+        this.centerX = 0;
+        this.centerY = 0;
+        this.sideLength = 100;
     }
 
     async initializeWorkloadModule(workerIndex, totalWorkers, roundIndex, roundArguments, sutAdapter, sutContext) {
         await super.initializeWorkloadModule(workerIndex, totalWorkers, roundIndex, roundArguments, sutAdapter, sutContext);
+        this.centerX = roundArguments.centerX || 0;
+        this.centerY = roundArguments.centerY || 0;
+        this.sideLength = roundArguments.sideLength || 100;
     }
 
     async submitTransaction() {
-        const entityID = `user${this.workerIndex}_${Math.floor(Math.random() * 1000)}`;
-        const antennaID = `Antenna${Math.floor(Math.random() * 100)}`;
-        const x = (Math.random() * 180 - 90).toFixed(4); // Random latitude [-90, 90]
-        const y = (Math.random() * 360 - 180).toFixed(4); // Random longitude [-180, 180]
-
+        const entityID = generateRandomID('user', 1000);
+        const antennaID = generateRandomID('Antenna', 100);
+        const { x, y } = generateRandomCoords(this.centerX, this.centerY, this.sideLength);
         const args = {
             contractId: this.chaincodeID,
-            contractFunction: 'AssignAntenna',
-            contractArguments: [entityID, antennaID, x, y],
+            contractFunction: 'CreateAsset',
+            contractArguments: [entityID, antennaID, x, y, '100'],
             readOnly: false
         };
-
         await this.sutAdapter.sendRequests({
             contractId: this.chaincodeID,
             channel: this.channel,
@@ -32,9 +38,7 @@ class LocationBasedAssignmentWorkload extends WorkloadModuleBase {
         });
     }
 
-    async cleanupWorkloadModule() {
-        // Cleanup logic if needed
-    }
+    async cleanupWorkloadModule() {}
 }
 
 function createWorkloadModule() {
