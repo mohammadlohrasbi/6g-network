66d211a881c29501965e01e34443ff1281c1d9ab
mohammadlohrasbi <46881172+mohammadlohrasbi@users.noreply.github.com>
2025-07-18 13:14:02 -0400

    Update utils.js

--- caliper-workspace/workload/utils.js
+++ caliper-workspace/workload/utils.js
@@ -1,11 +1,14 @@
+const seedrandom = require('seedrandom');
+
 const generateRandomID = (prefix, max) => {
     return `${prefix}${Math.floor(Math.random() * max)}`;
 };
 
-const generateRandomCoords = (centerX = 0, centerY = 0, sideLength = 100) => {
+const generateRandomCoords = (centerX = 0, centerY = 0, sideLength = 100, seed = null) => {
+    const rng = seed ? seedrandom(seed) : Math.random;
     const halfSide = sideLength / 2;
-    const x = (Math.random() * sideLength - halfSide + centerX).toFixed(4); // مختصات x در بازه [centerX - halfSide, centerX + halfSide]
-    const y = (Math.random() * sideLength - halfSide + centerY).toFixed(4); // مختصات y در بازه [centerY - halfSide, centerY + halfSide]
+    const x = (rng() * sideLength - halfSide + centerX).toFixed(4);
+    const y = (rng() * sideLength - halfSide + centerY).toFixed(4);
     return { x, y };
 };
 
