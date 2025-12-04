00d3f0ca143eab736cd42c7d330b71c0a9bce45c
mohammadlohrasbi <46881172+mohammadlohrasbi@users.noreply.github.com>
2025-12-04 18:09:30 -0500

    Update setup.sh

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -245,7 +245,7 @@ main() {
   generate_coreyamls
   start_network
   wait_for_orderer
-  fix_admin_ous          # حتماً قبل از ایجاد کانال!
+  # fix_admin_ous          # حتماً قبل از ایجاد کانال!
   create_and_join_channels
   package_and_install_chaincode
   approve_and_commit_chaincode
