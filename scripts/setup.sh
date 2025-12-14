4c76ac857c4f2a0e133479404af9077a5a351bc9
mohammadlohrasbi <46881172+mohammadlohrasbi@users.noreply.github.com>
2025-12-14 09:00:57 -0500

    Update setup.sh

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -132,7 +132,7 @@ create_and_join_channels() {
 
     # ایجاد کانال با MSP Org1MSP و TLS bundled
     if docker exec -e CORE_PEER_LOCALMSPID=Org1MSP \
-                   -e CORE_PEER_MSPCONFIGPATH=/etc/hyperledger/fabric/msp-users\
+                   -e CORE_PEER_MSPCONFIGPATH=/etc/hyperledger/fabric/shared-msp/Org1MSP \
                    -e CORE_PEER_ADDRESS=peer0.org1.example.com:7051 \
                    -e CORE_PEER_TLS_ENABLED=true \
                    -e CORE_PEER_TLS_ROOTCERT_FILE=/etc/hyperledger/fabric/bundled-tls-ca.pem \
@@ -173,7 +173,7 @@ create_and_join_channels() {
 
         # Join کردن با MSP سازمان خودش و TLS bundled
         if docker exec -e CORE_PEER_LOCALMSPID=Org${i}MSP \
-                       -e CORE_PEER_MSPCONFIGPATH=/etc/hyperledger/fabric/msp-users\
+                       -e CORE_PEER_MSPCONFIGPATH=/etc/hyperledger/fabric/shared-msp/Org${i}MSP \
                        -e CORE_PEER_ADDRESS=${PEER}:7051 \
                        -e CORE_PEER_TLS_ENABLED=true \
                        -e CORE_PEER_TLS_ROOTCERT_FILE=/etc/hyperledger/fabric/bundled-tls-ca.pem \
