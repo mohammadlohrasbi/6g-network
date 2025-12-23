3f3addc14b2e8daae626d57666118b07e9e827c6
mohammadlohrasbi <46881172+mohammadlohrasbi@users.noreply.github.com>
2025-12-23 07:49:03 -0500

    Update setup.sh

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -74,9 +74,6 @@ setup_network_with_fabric_ca_tls_nodeous_active() {
     cp "$TEMP_CRYPTO/peerOrganizations/${org}.example.com/ca/ca-${org}.${org}.example.com-cert.pem" "$CRYPTO_DIR/peerOrganizations/${org}.example.com/ca/ca-${org}.${org}.example.com-cert.pem"
     cp "$TEMP_CRYPTO/peerOrganizations/${org}.example.com/ca/"*_sk "$CRYPTO_DIR/peerOrganizations/${org}.example.com/ca/priv_sk"
 
-    cd "$TEMP_CRYPTO/peerOrganizations/${org}.example.com/tlsca"
-    tree
-    cd "$PROJECT_DIR"
 
     # نام درست TLS cert: tlsca.org${i}.example.com-cert.pem
     cp "$TEMP_CRYPTO/peerOrganizations/${org}.example.com/tlsca/tlsca-${org}.${org}.example.com-cert.pem" "$CRYPTO_DIR/peerOrganizations/${org}.example.com/tlsca/tlsca-${org}.example.com-cert.pem"
