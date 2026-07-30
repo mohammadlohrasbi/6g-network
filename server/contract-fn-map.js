c60246977f6bf1e71a3727c8027afef128ff10c4
mohammadlohrasbi <46881172+mohammadlohrasbi@users.noreply.github.com>
2026-07-30 05:46:28 -0400

    Update contract-fn-map.js

--- server/contract-fn-map.js
+++ server/contract-fn-map.js
@@ -94,4 +94,5 @@ const CONTRACT_FN = {
   UpdatePolicy: { fn: 'Update', params: ["policyID","policy"], antennaDep: false, needsSeed: false },
   VerifyIdentity: { fn: 'Verify', params: ["entityID","verified"], antennaDep: false, needsSeed: false },
 };
+
 module.exports = { CONTRACT_FN };
